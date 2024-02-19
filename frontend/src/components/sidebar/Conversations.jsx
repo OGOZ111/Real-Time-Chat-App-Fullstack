875914a851c6442d99e88b51764c3a8c2603174c
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-02-19 19:36:17 +0200

    installed Zustrand, created getCoversations hook to fetch all. tested working. Lost all git commits bc of a major git crash :(

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Conversation from "./Conversation";
+import useGetConvo from "../../hooks/useGetConversations.js";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConvo();
+  console.log("CONVERSATIONS:", conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto">
       <Conversation />
