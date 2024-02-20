b5117e14482312566432341f3b30cf772c795d06
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-02-20 21:51:10 +0200

    added sound notification function

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,19 +1,32 @@
 import React, { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
+import notificationSound from "../assets/sounds/notification1.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
 
   useEffect(() => {
-    socket?.on("newMessage", (newMessage) => {
+    const handleNewMessage = (newMessage) => {
       newMessage.shouldShake = true;
-      setMessages([...messages, newMessage]);
-    });
 
-    return () => socket?.off("newMessage");
+      const audio = new Audio(notificationSound);
+      audio.addEventListener("canplaythrough", () => {
+        audio.play();
+      });
+
+      setMessages([...messages, newMessage]);
+    };
+
+    socket?.on("newMessage", handleNewMessage);
+
+    return () => {
+      socket?.off("newMessage", handleNewMessage);
+    };
   }, [socket, setMessages, messages]);
+
+  return null;
 };
 
 export default useListenMessages;
